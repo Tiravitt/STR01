0a14aad488fa83d149c977667334b7eb5279a030
Tiravitt <tiravitfrang11@gmail.com>
2023-12-15 15:57:30 +0700

    .commit

--- home.py
+++ home.py
@@ -2,10 +2,6 @@ import streamlit as st
 
 st.balloons()
 
-lot3="https://lottie.host/9d7858db-0b59-4395-ae0f-b57203577235/vTJKGelBz0.json"
-lottie3 = load_lottieurl(lot3)
-st_lottie(lottie3)
-
 st.header("Tiravit thongkham")
 
 st.title('การทดสอบเขียนเว็บด้วย Python')
