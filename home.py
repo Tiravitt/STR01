1c0ee1df1457da540856cf1a5edbf764cdb48a2a
Tiravitt <tiravitfrang11@gmail.com>
2023-12-15 14:22:45 +0700

    .commit

--- home.py
+++ home.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.header("Tiravit thongkham")
+
+st.title('การทดสอบเขียนเว็บด้วย Python')
+st.header('ฐิรวิชญ์ ทองคำ')
+st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
+st.markdown("----")
