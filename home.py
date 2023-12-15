463744a27d3f6633e376dd83f14fad9b05c57907
Tiravitt <tiravitfrang11@gmail.com>
2023-12-15 14:49:34 +0700

    .commit

--- home.py
+++ home.py
@@ -13,7 +13,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.image('./pic/me.jpg')
 with col2:
-    st.image('./pic/iris.jpg')
+    st.image('./pic/sky.jpg')
 
 html_1 = """
 <div style="background-color:#001BFB;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
@@ -27,3 +27,17 @@ import pandas as pd
 
 dt=pd.read_csv('./data/iris.csv')
 st.write(dt.head(10))
+
+dt1 = dt['petal.length'].sum()
+dt2 = dt['petal.width'].sum()
+dt3 = dt['sepal.length'].sum()
+dt4 = dt['sepal.width'].sum()
+
+dx = [dt1, dt2, dt3, dt4]
+dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+
+if st.button("show bar chart"):
+    st.bar_chart(dx2)
+    st.button("Not show bar chart")
+else :
+    st.button("Not show bar chart") 
