6762fdeadb12fe3740fa0a8aecd662956b74fe50
Tiravitt <tiravitfrang11@gmail.com>
2023-12-15 14:28:15 +0700

    .commit

--- home.py
+++ home.py
@@ -6,3 +6,11 @@ st.title('การทดสอบเขียนเว็บด้วย Python
 st.header('ฐิรวิชญ์ ทองคำ')
 st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
 st.markdown("----")
+
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    st.image('./pic/me.jpg')
+with col2:
+    st.image('./pic/iris.jpg')
