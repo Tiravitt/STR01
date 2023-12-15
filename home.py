3d092e5caa59748b57fbc3737d60393e65a5bfbf
Tiravitt <tiravitfrang11@gmail.com>
2023-12-15 14:39:46 +0700

    .commit

--- home.py
+++ home.py
@@ -16,8 +16,8 @@ with col2:
     st.image('./pic/iris.jpg')
 
 html_1 = """
-<div style="background-color:#001BFB;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:white">
-<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+<div style="background-color:#001BFB;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5 style= 'color:white'>สถิติข้อมูลดอกไม้</h5></center>
 </div>
 """
 st.markdown(html_1, unsafe_allow_html=True)
