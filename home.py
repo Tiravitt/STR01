f9757b5d29273e1a1444d329c37956c32e4987a6
Tiravitt <tiravitfrang11@gmail.com>
2023-12-15 15:04:02 +0700

    .commit

--- home.py
+++ home.py
@@ -76,14 +76,14 @@ if st.button("ทำนายผล"):
    out=Knn_model.predict(x_input)
 
    if out[0]=="Setosa":
-      #st.image("./pic/iris.jpg")
-      st.header("Setosa")
+      st.image("./pic/1.jpg")
+      #st.header("Setosa")
    elif out[0]=="Versicolor":
-      #st.image("./pic/iris2.jpg")
-      st.header("Versicolor")
+      st.image("./pic/2.jpg")
+      #st.header("Versicolor")
    else:
-      #st.image("./pic/iris1.jpg")  
-      st.header("Verginiga")
+      st.image("./pic/3.jpg")  
+      #st.header("Verginiga")
    st.button("ไม่ทำนายผล")
 else :
     st.button("ไม่ทำนายผล")
